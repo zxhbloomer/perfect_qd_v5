38ae367053f872135fa88e239a838c83dff8d023
zxhbloomer@gmail.com <Lyyzxh030324>
2020-09-09 16:15:59 +0800

    ..

--- src/layout/components/TopNav/index.vue
+++ src/layout/components/TopNav/index.vue
@@ -61,7 +61,6 @@ export default {
   },
   methods: {
     handleSelect(key, keyPath) {
-      debugger
       if (this.permission_topNav[key - 1].index === this.activeIndex) {
         return
       }
